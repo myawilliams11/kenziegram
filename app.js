a36ec157ba6bbcfe11912971c3cb20c51cd12697
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-04-26 11:22:42 -0400

    Add files via upload
    final

--- app.js
+++ app.js
@@ -10,13 +10,33 @@ const uploaded_files = [];
 app.use(express.static('public'));
 app.use(express.static('public/uploads/'));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     const path = './public/uploads';
     fs.readdir(path, function(err,items) {
         console.log(items);
-        let html= `<h1>Welcome to Kenziegram!</h1>`;
+        let html= `
+            <style>
+            </style>
+            <h1>Welcome to Kenziegram!</h1>
+            <form action="http://localhost:3000/upload" method="post" enctype="multipart/form-data">
+            <div>
+              <label for="file">Choose a File</label>
+              <!-- <input type="file" id="file" name="myFile"> -->
+              <input type="file" id="file" name="myFile">
+              
+              
+            </div>
+            <div>
+              <button>Send the file</button>
+            </div>
+          </form>
+
+            
+        `;
+        // the href takes to upload page
         for (i=0; i<items.length; i++){
-            html+= `<img src="${items[i]}">`;
+            html+= `<img src="${items[i]}" width=300px height=300px>`;
+            
         }
         res.send(html);
     });
@@ -28,9 +48,12 @@ app.post('/upload', upload.single('myFile'), function (req, res, next) {
     // req.body will hold the text fields, if there were any
     console.log("Uploaded: " + req.file.filename);
     uploaded_files.push(req.file.filename);
-    res.end("Uploaded file!");
+    res.send('<a href="/">Upload Form</a>');
+    // res.setHeader("Location", "http://localhost:3000/home")
   })
 
+
+  
 
 
 app.listen(port);
